1f1ba81db4f7d5c32b601bed4d9d451e0df3ef74
Eric Butler <neptunecentury@hotmail.com>
2014-11-24 16:17:44 -0500

    add unit test

---
+++ FMODApp.Tests/SoundEngineUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using FMODApp.Engine;
+
+
+namespace FMODApp.Tests
+{
+    [TestClass]
+    public class SoundEngineUnitTest
+    {
+        [TestMethod]
+        public void TestCreateSoundSystem()
+        {
+            //test creating and destroying a sound system.
+            var ss = new SoundEngine();
+
+        }
+    }
+}
