2bc12fde0915bd21b239a35f576a17f4a43a6223
Eric Butler <neptunecentury@hotmail.com>
2014-11-24 16:17:08 -0500

    commiting fmod lib and unit test

---
+++ FMODApp.Engine/SoundEngine.cpp
@@ -0,0 +1,18 @@
+﻿// Class1.cpp
+#include "pch.h"
+#include "SoundEngine.h"
+
+using namespace FMODApp::Engine;//::Engine;
+using namespace Platform;
+
+//constructor
+SoundEngine::SoundEngine()
+{
+	//start up the FMOD system
+	FMOD_RESULT result = FMOD_System_Create(this->system);
+	//check the result
+	if (result != FMOD_OK){
+		throw ref new Exception(0, "FMOD failed to initialize");
+	}
+}
+
---
+++ FMODApp.Engine/SoundEngine.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "FMOD\fmod.h"
+//namespace FMODApp_Engine{}
+
+namespace FMODApp//_Engine
+{
+	namespace Engine{
+		public ref class SoundEngine sealed
+		{
+
+		private:
+			FMOD_SYSTEM **system;
+
+		public:
+			SoundEngine();
+			
+		};
+	}
+}
